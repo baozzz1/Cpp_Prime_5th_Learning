97e9aaba054163fbd255515c626a57a707159428
鲍建军 <baojianjun786@outlook.com>
2018-09-30 20:54:35 +0800

    Finished Reading Chapter 16.

--- Cpp_Prime_5th_Learning/Lesson16.cpp
+++ Cpp_Prime_5th_Learning/Lesson16.cpp
@@ -1,8 +1,57 @@
 #include "Lesson16.h"
+#include"Lesson14.h"
 using namespace std;
 
-#include"Lesson14.h"
 class Sales_data;
+
+template <typename T> 
+Blob<T>::Blob() : data(make_shared<vector<T>>()) {}
+
+template <typename T> 
+Blob<T>::Blob(initializer_list<T> il) : data(make_shared<vector<T>>(il)) {}
+
+template <typename T>
+void Blob<T>::check(size_type i, const string &msg) const {
+	if (i >= data->size())
+		throw out_of_range(msg);
+}
+
+template <typename T>
+T& Blob<T>::back() {
+	check(0, "back on empty Blob");
+	return data->back();
+}
+template <typename T>
+const T& Blob<T>::back() const {
+	check(0, "back on empty Blob");
+	return data->back();
+}
+template <typename T>
+T& Blob<T>::operator[](size_type i) {
+	check(i, "subscript out of range");
+	return (*data)[i];
+}
+template <typename T>
+const T& Blob<T>::operator[](size_type i) const {
+	check(i, "subscript out of range");
+	return (*data)[i];
+}
+template <typename T>
+void Blob<T>::pop_back() {
+	check(0, "pop_back on empty Blob");
+	data->pop_back();
+}
+
+
+template <typename T>
+BlobPtr<T>& BlobPtr<T>::operator++() {
+	BlobPtr ret = *this;
+	++*this;
+	return ret;
+}
+
+
+
 
 void runLesson16() {
 	vector<int> vec = { 0,1,2,3,4,5,6,7,8,9 };
--- Cpp_Prime_5th_Learning/Lesson16.h
+++ Cpp_Prime_5th_Learning/Lesson16.h
@@ -8,10 +8,10 @@
 #include<list>
 #include<algorithm>
 
-template <typename T>
-int compare(const T &v1, const T &v2) {
-	if (v1 < v2) return -1;
-	if (v2 < v1) return 1;
+template <typename T, typename F = std::less<T>>
+int compare(const T &v1, const T &v2, F f = F()) {
+	if (f(v1, v2)) return -1;
+	if (f(v2, v1)) return 1;
 	return 0;
 }
 
@@ -25,4 +25,56 @@ T func16_4(T beg, T end, const U &target) {
 	}
 	return end;
 }
+
+template <typename> class BlobPtr;
+template <typename T>
+bool operator==(const Blob<T>&, const Blob<T>&);
+
+template <typename T> class Blob {
+	friend class BlobPtr<T>;
+	friend bool operator==<T>(const Blob<T>&, const Blob<T>&);
+public:
+	typedef T value_type;
+	typedef typename std::vector<T>::size_type size_type;
+	//构造函数
+	Blob();
+	Blob(std::initializer_list<T> il);
+	//Blob中的元素数目
+	size_type size() const { return data->size(); }
+	bool empty() const { return data->empty(); }
+	//添加和删除元素
+	void push_back(const T &t) { data->push_back(t); }
+	//移动版本
+	void push_back(T &&t) { data->push_back(std::move(t)); }
+	void pop_back();
+	//元素访问
+	T& back();
+	const T& back() const;
+	T& operator[](size_type i);
+	const T& operator[](size_type i) const;
+
+private:
+	std::shared_ptr<std::vector<T>> data;
+	void check(size_type i, const std::string &msg) const;
+};
+
+template <typename T> class BlobPtr {
+public:
+	BlobPtr() :curr(0) {}
+	BlobPtr(Blob<T> &a, size_t sz = 0) :
+		wptr(a.data), curr(sz) {}
+	T& operator*() const {
+		auto p = check(curr, "dereference past end");
+		return (*p)[curr];
+	}
+	BlobPtr& operator++();
+	BlobPtr& operator--();
+
+private:
+	std::shared_prt<std::vector<T>>
+		check(std::size_t, const std::string&) const;
+	std::weak_ptr<std::vector<T>> wptr;
+	std::size_t curr;
+};
+
 #endif // ! _LESSON16_
